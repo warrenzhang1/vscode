f31a9a2647edaa1254f0bd049c5ca97433c85227
Warren Zhang <warrenzhang989@gmail.com>
2023-01-04 14:40:50 -0800

    changed it to only print most frequent pattern

--- bioinformatics algorithms/count_k_mer.py
+++ bioinformatics algorithms/count_k_mer.py
@@ -3,7 +3,11 @@ user_text = input('What is your text?')
 user_k = int(input('How many k-mers?'))
 user_dict = frequent(user_text, user_k)
 print(user_dict)
-for key, value in user_dict.items() :
-    count = 0 
-    if value > count :
+
+sorted_user_dict = dict(sorted(user_dict.items(), key = lambda x:x[1], reverse = True ))
+maximum_value  = (list(sorted_user_dict.values())[0])
+
+for key, value in sorted_user_dict.items() :
+    
+    if maximum_value == value :
         print (key + " " + str(value))
